94e1abe4f08de013b6b6ba0e3881409afe95eb6b
David Fague <98128717+davidfague@users.noreply.github.com>
2023-03-27 20:59:59 -0500

    Update cable_expander_func.py

--- cable_expander_func.py
+++ cable_expander_func.py
@@ -10,7 +10,7 @@ from decimal import Decimal
 import numpy as np
 import neuron
 from neuron import h
-from neuron_reduce.subtree_reductor_func import load_model, gather_subtrees, mark_subtree_sections_with_subtree_index, create_segments_to_mech_vals,_get_subtree_biophysical_properties
+from neuron_reduce.subtree_reductor_func import load_model, gather_subtrees, mark_subtree_sections_with_subtree_index, create_segments_to_mech_vals
 from neuron_reduce.reducing_methods import _get_subtree_biophysical_properties
 h.load_file("stdrun.hoc")
 
