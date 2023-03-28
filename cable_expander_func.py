23759fd2bf23f59454f7835f77f40db21c0c7c15
David Fague <98128717+davidfague@users.noreply.github.com>
2023-03-27 22:00:15 -0500

    Update cable_expander_func.py

--- cable_expander_func.py
+++ cable_expander_func.py
@@ -12,6 +12,7 @@ import neuron
 from neuron import h
 from neuron_reduce.subtree_reductor_func import (load_model, gather_subtrees, mark_subtree_sections_with_subtree_index, create_segments_to_mech_vals, 
                                                  calculate_nsegs_from_lambda, create_sections_in_hoc, append_to_section_lists, calculate_subtree_q,
+                                                 type_of_point_process,
                                                  Neuron)
 from neuron_reduce.reducing_methods import (_get_subtree_biophysical_properties, measure_input_impedance_of_subtree, find_lowest_subtree_impedance, 
                                             find_space_const_in_cm, push_section, find_best_real_X)
