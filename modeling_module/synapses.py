a1cc6c8aa98400b6ab5927f43763b6e5f11ad4b5
David Fague <98128717+davidfague@users.noreply.github.com>
2023-04-18 16:44:04 -0500

    Update synapses.py

--- modeling_module/synapses.py
+++ modeling_module/synapses.py
@@ -36,10 +36,11 @@ class PointCurrent(ABC):
         iseg = int(self.get_segment().x * self.get_section().nseg)
         return self.cell.sec_id_in_seg[self.sec_index] + iseg
 class CurrentInjection(PointCurrent):
-    """A module for current injection"""
+    """A module for current injection
+    was current: Optional[np.ndarray, List[int]] = None,"""
 
     def __init__(self, cell, sec_index: int, loc: float = 0.5,
-                 pulse: bool = True, current: Optional[np.ndarray] = None,  # was current: Optional[np.ndarray, List[int]] = None,
+                 pulse: bool = True, current: Optional[np.ndarray] = None,
                  dt: Optional[np.ndarray] = None, record: bool = False, **pulse_param: Any) -> None:
         """
         cell: target cell object
