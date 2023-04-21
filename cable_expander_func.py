bf30d66a4696da6084a406562022fe7c92a60aff
David Fague <98128717+davidfague@users.noreply.github.com>
2023-04-20 21:20:06 -0500

    Update cable_expander_func.py

--- cable_expander_func.py
+++ cable_expander_func.py
@@ -1064,7 +1064,7 @@ def duplicate_synapse(synapse,PP_params_dict):
     new_synapse = eval('h.'+syn_type)(seg)
             # extract the parameters using hoc instead of PyNeuronToolbox
     for param_name in dir(synapse):
-        if param_name in PP_param_dict[syn_type]:
+        if param_name in PP_params_dict[syn_type]:
             param_value = getattr(synapse, param_name)
             if hasattr(new_synapse, param_name):
                 setattr(new_synapse, param_name, param_value)
