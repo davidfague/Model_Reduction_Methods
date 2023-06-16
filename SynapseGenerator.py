e260ddc0e20942a16cf5c7cdf775ac48f0a4ec12
David Fague <98128717+davidfague@users.noreply.github.com>
2023-06-16 14:49:56 -0500

    Update SynapseGenerator.py

--- SynapseGenerator.py
+++ SynapseGenerator.py
@@ -5,49 +5,52 @@ class SynapseGenerator:
     def __init__(self):
         self.synapses = [] # list of lists of synapses that were generated using this class
         
-    def add_synapses(self, segments, probs=None, 
-                     exc_density=None, inh_density=None,
-                     total_synapses=int, ratio=None):
+    def add_synapses(self, segments, gmax, syn_mod = str,
+                     probs=None,
+                     density=None,
+                     number_of_synapses=int,
+                     record: bool = False):
       '''
-      Creates a list of synapses by specifying excitatory and inhibitory densities, or number of synapses and ratio
+      Creates a list of synapses by specifying density or number of synapses
       --------------------------------------------------------------------------------------------------------
       segments: list of neuron segments to choose from
+      gmax: float or distribution to sample from
+      syn_mod:  str of synapse modfile ex. 'AMPA_NMDA', 'pyr2pyr', 'GABA_AB', 'int2pyr'
       probs: list of probabilities of choosing each segment -- if not provided, segment_length/total_segment_length will be used
-      exc_density: density of excitatory synapses in synapses/micron
-      inh_density: density of inhibitory synapses in synapses/micron
-      total_synapses: integar number of synapses to distribute to the cell -- Optional
-      ratio: proporiton of synapses that are excitatory (0 to 1)
+      density: density of excitatory synapses in synapses/micron 
+      number_of_synapses: int number of synapses to distribute to the cell
+      record: bool whether or not to record synapse currents
       '''
       synapses=[]
       
       # Error checking
-      if (exc_density is not None and inh_density is not None) and (total_synapses is not None or ratio is not None):
-        raise ValueError('Cannot specify both densities and total_synapses/ratio')
+      if (density is not None) and (number_of_synapses is not None):
+        raise ValueError('Cannot specify both density and number_of_synapses')
 
       # Calculate probabilities if not given
       if not probs:
           total_length = sum([seg.sec.L/seg.sec.nseg for seg in segments])
           probs = [(seg.sec.L/seg.sec.nseg)/total_length for seg in segments]
 
-      # Calculate total synapses if given densities
-      if exc_density and inh_density:
+      # Calculate number of synapses if given densities
+      if density:
           total_length = sum([seg.sec.L/seg.sec.nseg for seg in segments])
-          total_synapses = total_length * (exc_density + inh_density)
-          ratio = exc_density / inh_density
-
-      # Calculate number of excitatory and inhibitory synapses
-      num_exc_synapses = round(total_synapses * ratio / (1 + ratio))
-      num_inh_synapses = total_synapses - num_exc_synapses
+          number_of_synapses = int(total_length * density)
 
       # Add synapses
-      for _ in range(num_exc_synapses):
-          segment = random.choices(segments, probs)[0]
-          synapses.append(Synapse(segment, 'excitatory'))
-
-      for _ in range(num_inh_synapses):
-          segment = random.choices(segments, probs)[0]
-          synapses.append(Synapse(segment, 'inhibitory'))
-          
+      if callable(gmax): # gmax is distribution
+          for _ in range(number_of_synapses):
+              segment = random.choices(segments, probs)[0]
+              synapses.append(Synapse(segment,
+                      syn_mod = syn_mod, gmax = gmax(size=1),
+                      record = record)
+      else: # gmax is float
+          for _ in range(number_of_synapses):
+              segment = random.choices(segments, probs)[0]
+              synapses.append(Synapse(segment,
+                      syn_mod = syn_mod, gmax = gmax,
+                      record = record)
+                             
       self.synapses.append(synapses)
 
       return synapses
